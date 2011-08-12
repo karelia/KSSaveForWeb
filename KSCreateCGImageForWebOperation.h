4414dd2ca42ddd303ed68aa8745400d31211efb2
Mike Abdullah <me@mikeabdullah.net>
2011-08-12 21:19:52 +0100

    comments

--- KSCreateCGImageForWebOperation.h
+++ KSCreateCGImageForWebOperation.h
@@ -17,7 +17,8 @@
     CGImageRef  _result;
 }
 
-// If context is nil, operation will create its own. It's best if you can pass one in since contexts cache their resources. If you do, I'm fairly sure contexts are only safe to be used from a single thread at a time, so make sure you place all such operations on the same queue. It is assumed the context has been set up in sRGB colorspace
+// If context is nil, operation will create its own. It's best if you can pass one in since contexts cache their resources
+// If providing your own context, I'm fairly sure they are only safe to be used from a single thread at a time, so make sure you place all such operations on the same queue. In general set context up to use sRGB colorspace, the .m file shows how to do that
 - (id)initWithCIImage:(CIImage *)image context:(CIContext *)context;
 
 @property(nonatomic, readonly) CGImageRef CGImage;  // NOT KVO-compliant, observe isFinished instead
